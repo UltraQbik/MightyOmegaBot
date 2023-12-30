80b9243fb3be2491f4af92345994fb9a6b81ae14
UltraQbik <no1skill@yandex.ru>
2023-12-30 03:04:28 +0300

    Revert "Poorly tested commit 👍"
    This reverts commit c89ee848a4f31df1a19d4d9ed469f969d57124ed.

--- client_cogs/Youtube_Notifications/main.py
+++ client_cogs/Youtube_Notifications/main.py
@@ -1,66 +1,57 @@
 import scrapetube
-import clientconfig
-import configparser
 from discord.ext import commands, tasks
+
+
+def get_yt_channels() -> dict[str, str]:
+    with open("client_cogs/Youtube_Notifications/channels.txt", "r") as file:
+        channel_list = file.read().replace("\r\n", "\n").split("\n")
+        channels = {}
+        for entry in channel_list:
+            if not entry:
+                continue
+            channel_name, channel_url = entry.split(" ")
+            channels[channel_name] = channel_url
+    return channels
 
 
 class EXTYoutubeModule(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-        self.config: None | dict[str, configparser.ConfigParser] = clientconfig.get_config()
-        self.channels: dict[str, list] = {}
+        self.channels: dict[str, str] = get_yt_channels()
         self.videos: dict[str, list[str]] = {}
 
         self.check.start()
 
-    @staticmethod
-    def parse_channel_list(channel_list: str):
-        # list looks like this
-        # [NAME] [URL],[NAME] [URL],[NAME] [URL],...
-        # split by ',' to get list of '[NAME] [URL]'
-        # split sub-lists by ' ' to get individual names and urls of channels
-
-        parsed = []
-        list_ = channel_list.split(",")
-        for item in list_:
-            channel = item.split(" ")
-            parsed.append({"name": channel[0], "url": channel[1]})
-        return parsed
-
     @tasks.loop(minutes=1)
     async def check(self):
-        for guild in self.client.guilds:
-            # fetch news channel
-            guild_config = self.config["youtube"][guild.id.__str__()]
-            if self.check.current_loop == 1:
-                self.channels[guild.id.__str__()] = self.parse_channel_list(guild_config["NotificationChannelList"])
+        """ Checks every minute for a new video, from channels in 'channels.txt' """
 
-            discord_channel = self.client.get_channel(int(guild_config["NotificationNewsChannel"]))
+        # Unnecessary; Update channel list every minute
+        # self.channels = get_yt_channels()
+        discord_channel = self.client.get_channel(863394227655933994)
 
-            # go through all listed to check channels
-            for channel in self.channels[guild.id.__str__()]:
-                # get their videos
-                videos = scrapetube.get_channel(channel_url=channel["url"], limit=5)
-                video_ids = [video["videoId"] for video in videos]
+        for channel_name, channel_url in self.channels.items():
+            videos = scrapetube.get_channel(channel_url=channel_url, limit=5)
+            video_ids = [video["videoId"] for video in videos]
 
-                # if the module was just activated
-                if self.check.current_loop == 1:
-                    self.videos[channel["url"]] = video_ids
-                    continue
+            if self.check.current_loop == 0:
+                self.videos[channel_name] = video_ids
+                continue
 
-                # go through videos, and post new ones
-                for video_id in video_ids:
-                    if video_id not in self.videos[channel["url"]]:
-                        url = f"https://youtu.be/{video_id}"
+            for video_id in video_ids:
+                if video_id not in self.videos[channel_name]:
+                    url = f"https://youtu.be/{video_id}"
 
-                        pings = "".join([f"<@&{pid}>" for pid in guild_config["NotificationRoles"].split(",")])
-                        await discord_channel.send(
-                            guild_config["NotificationNewsMessage"].replace("\\n", "\n").format(
-                                ping=pings, name=channel["name"], url=url)
-                        )
+                    # ye, roles are not fully supported just yet
+                    await discord_channel.send(
+                        f"<@&1101231667554828410><@&1110299526721449984>\n"
+                        f"\n"
+                        f"# :fire: Новое видео на канале {channel_name}! :fire:\n"
+                        f"# [Ссылочка в сибирь :>]({url})"
+                    )
 
-                self.videos[channel["url"]] = video_ids
+            self.videos[channel_name] = video_ids
 
 
 async def setup(client: commands.Bot) -> None:
