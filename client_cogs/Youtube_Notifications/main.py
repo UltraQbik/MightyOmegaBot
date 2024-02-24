8793db673a40dfcf29c26cd7765ca6f6c5199ff9
Nakidai <alex_platonov1@mail.ru>
2024-02-24 16:38:37 +0300

    Improve Youtube_Notifications module

--- client_cogs/Youtube_Notifications/main.py
+++ client_cogs/Youtube_Notifications/main.py
@@ -2,60 +2,76 @@ import scrapetube
 from discord.ext import commands, tasks
 
 
-def get_yt_channels() -> dict[str, str]:
-    with open("client_cogs/Youtube_Notifications/channels.txt", "r") as file:
-        channel_list = file.read().replace("\r\n", "\n").split("\n")
-        channels = {}
-        for entry in channel_list:
-            if not entry:
-                continue
-            channel_name, channel_url = entry.split(" ")
-            channels[channel_name] = channel_url
-    return channels
+def read_config() -> dict[int, dict[str, dict[str, str]]]:
+    with open("client_configs/youtube_notifier.json") as file:
+        config = json.load(file)
+    return config
 
 
 class EXTYoutubeModule(commands.Cog):
+    """
+    Class that checks every minute for a new video.
+    It has config in client_configs/youtube_notofier.json, which looks
+    something like this:
+    {
+        discord_channel_id:
+        {
+            "Chanel_name":
+            {
+                "url": "URL to channel",
+                "video": "Message with mention"
+            }
+        }
+    }
+
+    In "video" you can also use "{0}" (channel name) and "{1}" (video URL).
+    """
+
     def __init__(self, client: commands.Bot):
         self.client = client
 
-        self.channels: dict[str, str] = get_yt_channels()
-        self.videos: dict[str, list[str]] = {}
+        self.config: dict[int, dict[str, dict[str, str]]] = read_config()
+        self.videos: dict[int, dict[str, list[str]]] = {}
+
+        self.discord_channels = []
+        for channel_id in self.config:
+            self.discord_channels.append(self.client.get_channel(channel_id))
 
         self.check.start()
 
     @tasks.loop(minutes=1)
     async def check(self):
-        """ Checks every minute for a new video, from channels in 'channels.txt' """
+        """ Checks every minute for a new video """
 
-        # Unnecessary; Update channel list every minute
-        # self.channels = get_yt_channels()
-        discord_channel = self.client.get_channel(863394227655933994)
+        # Unnecessary; Update config every minute
+        # self.config = read_config()
 
-        for channel_name, channel_url in self.channels.items():
-            videos = scrapetube.get_channel(channel_url=channel_url, limit=5)
-            try:
-                video_ids = [video["videoId"] for video in videos]
-            except Exception as e:
-                print(f"Another BS error from somewhere: {e}")
-                continue
+        for channel in self.discord_channels:
+            for channel_name in self.config[channel.id]:
+                videos = scrapetube.get_channel(
+                    channel_url=self.config[channel][channel_name]["url"],
+                    limit=5
+                )
+                try:
+                    video_ids = [video["videoId"] for video in videos]
+                except Exception as e:
+                    print(f"Another BS error from somewhere: {e}")
+                    continue
 
-            if self.check.current_loop == 0:
-                self.videos[channel_name] = video_ids
-                continue
+                if self.check.current_loop == 0:
+                    self.videos[channel.id][channel_name] = video_ids
 
-            for video_id in video_ids:
-                if video_id not in self.videos[channel_name]:
+                for video_id in video_ids:
+                    if video_id in self.videos[channel.id][channel_name]:
+                        continue
                     url = f"https://youtu.be/{video_id}"
 
-                    # ye, roles are not fully supported just yet
                     await discord_channel.send(
-                        f"<@&1101231667554828410><@&1110299526721449984>\n"
-                        f"\n"
-                        f"# :fire: Новое видео на канале {channel_name}! :fire:\n"
-                        f"# [Ссылочка в сибирь :>]({url})"
+                        self.config[channel.id][channel_name]["video"].format(
+                            channel_name, url
+                        )
                     )
-
-            self.videos[channel_name] = video_ids
+                self.videos[channel.id][channel.name] = video_ids
 
 
 async def setup(client: commands.Bot) -> None:
