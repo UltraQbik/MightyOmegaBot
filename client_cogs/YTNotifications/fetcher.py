dd86321204a5a95c36b4d9b8c7ddf88e0f2bc5af
UltraQbik <no1skill@yandex.ru>
2024-07-26 16:48:18 +0300

    Maybe working YTNotifs

--- client_cogs/YTNotifications/fetcher.py
+++ client_cogs/YTNotifications/fetcher.py
@@ -3,14 +3,32 @@ import requests
 import xml.etree.ElementTree as et
 
 
-def fetch_videos(channel_id: str, num: int):
+def fetch_xml(channel_id: str) -> et.Element:
+    """
+    Returns YouTubes feed XML element
+    :param channel_id: channel id
+    :return: XML Element
+    """
+
     channel_url = f'https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}'
 
     # get request
     r = requests.get(channel_url)
 
     # generate XML tree
-    tree: et.Element = et.fromstring(r.content)
+    return et.fromstring(r.content)
+
+
+def fetch_videos(channel_id: str, num: int) -> list[str]:
+    """
+    Returns a list of N last videos posted on the channel
+    :param channel_id: channel id
+    :param num: number of videos to fetch (max 20)
+    :return: list of video's id's
+    """
+
+    # fetch XML tree
+    tree = fetch_xml(channel_id)
 
     # xml namespace (I guess?)
     namespace = re.findall(r"\{(.*?)\}", tree.tag)[0]
@@ -23,3 +41,24 @@ def fetch_videos(channel_id: str, num: int):
 
     # return videos
     return videos
+
+
+def fetch_channel_name(channel_id: str) -> str:
+    """
+    Returns channel name string.
+    :param channel_id: channel id
+    :return: channel name
+    """
+
+    # fetch XML tree
+    tree = fetch_xml(channel_id)
+
+    # xml namespace (I guess?)
+    namespace = re.findall(r"\{(.*?)\}", tree.tag)[0]
+
+    # find author tag
+    author = tree.find("author", {"": namespace})
+
+    # return channel name
+    # zeroth element should be a name, if it's not, oh well
+    return author[0].text
--- client_cogs/YTNotifications/main.py
+++ client_cogs/YTNotifications/main.py
@@ -7,7 +7,7 @@ Assumed problem was ScrapeTube library that was used, that's why in this one the
 import json
 import configparser
 from discord.ext import commands, tasks
-from client_cogs.YTNotifications.fetcher import fetch_videos
+from client_cogs.YTNotifications.fetcher import fetch_videos, fetch_channel_name
 
 
 class EXTYoutubeModule(commands.Cog):
@@ -21,10 +21,12 @@ class EXTYoutubeModule(commands.Cog):
         self.client = client
 
         # cog's config file
-        self.notifs_cfg: dict[int, dict[str, int | list[str]]] = {}
+        self.notifs_cfg: dict[int, dict[str, str | int | list[str]]] = {}
         # {
         #   [guildId]: {
         #     "newsChannel": [channelId],
+        #     "pingRole": [roleId],
+        #     "pingMsg": [string],
         #     "channels": [[ytchannelId], [ytchannelId], [ytchannelId]]
         #   }
         # }
@@ -36,7 +38,7 @@ class EXTYoutubeModule(commands.Cog):
         # }
 
         self.load_configs()
-        # self.check.start()
+        self.check.start()
 
     def load_configs(self):
         """
@@ -55,6 +57,8 @@ class EXTYoutubeModule(commands.Cog):
             channels = json.loads(cfg[guild_id]["channels"])
             self.notifs_cfg[int(guild_id)] = {
                 "newsChannel": int(cfg[guild_id]["newsChannel"]),
+                "pingRole": int(cfg[guild_id]["pingRole"]),
+                "pingMsg": cfg[guild_id]["pingMsg"],
                 "channels": channels
             }
             channel_urls.update(channels)
@@ -67,7 +71,40 @@ class EXTYoutubeModule(commands.Cog):
     async def check(self):
         """ Checks every 5 minutes for a new video"""
 
-        pass
+        # go through all guilds the bot is in
+        for guild in self.client.guilds:
+            # skip guild, if it's not in notifications config
+            if guild.id not in self.notifs_cfg:
+                continue
+
+            news_channel = self.client.get_channel(self.notifs_cfg[guild.id]["newsChannel"])
+
+            # skip if something wasn't configured properly
+            # and print a message
+            if news_channel is None:
+                print("WARN: YTNotifs: news channel is not configured properly")
+                continue
+
+            # go through channels, and check for new videos
+            for channel in self.notifs_cfg[guild.id]["channels"]:
+                # fetch 5 last videos
+                last_videos = fetch_videos(channel, 5)
+
+                # go through videos, and check if they are all in the database
+                for video in last_videos:
+                    # if video is not in the database, make a notification about it
+                    if video not in self.channels[channel]:
+                        message = self.notifs_cfg[guild.id]["pingMsg"]
+                        message.format(
+                            ping=f'<@&{self.notifs_cfg[guild.id]["pingRole"]}>',
+                            channel_name=fetch_channel_name(channel),
+                            video_link=f'https://www.youtube.com/watch?v={video}'
+                        )
+
+                        await news_channel.send(message)
+
+                # update the database
+                self.channels[channel] = last_videos
 
 
 async def setup(client: commands.Bot) -> None:
