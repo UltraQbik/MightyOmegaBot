eb7e5c0774e4b1c9f8b648ce91fba3eeabba53f4
UltraQbik <no1skill@yandex.ru>
2024-08-15 01:56:11 +0300

    Status bot cog Get rid of match case as it adds unnecessary white space

--- client_cogs/Status/main.py
+++ client_cogs/Status/main.py
@@ -38,38 +38,41 @@ class EXTStatus(commands.Cog):
         This is an example slash command
         """
 
-        match choices.value:
-            case "bot":
-                ping = self.client.latency
+        if choices.value == "bot":
+            ping = self.client.latency
 
-                # uptime stuff
-                uptime = (datetime.now() - self.start_time).total_seconds()
-                minutes = uptime / 60
-                hours = minutes / 60
-                days = hours / 24
-                weeks = days / 7
-                months = days / 30
+            # uptime stuff
+            uptime = (datetime.now() - self.start_time).total_seconds()
+            minutes = uptime / 60
+            hours = minutes / 60
+            days = hours / 24
+            weeks = days / 7
+            months = days / 30
 
-                uptime_str = ""
-                if int(uptime) % 60 > 0:
-                    uptime_str = f"{int(uptime) % 60} second{'' if int(uptime) % 60 == 1 else 's'}"
-                if int(minutes) % 60 > 0:
-                    uptime_str = f"{int(minutes) % 60} minute{'' if int(minutes) % 60 == 1 else 's'} " + uptime_str
-                if int(hours) % 24 > 0:
-                    uptime_str = f"{int(hours) % 24} hour{'' if int(hours) % 24 == 1 else 's'} " + uptime_str
-                if int(days) % 60 > 0:
-                    uptime_str = f"{int(days) % 7} day{'' if int(days) % 7 == 1 else 's'} " + uptime_str
-                if int(weeks) % 4.28 > 0:
-                    uptime_str = f"{int(weeks) % 4.28} week{'' if int(weeks) % 4.28 == 1 else 's'} " + uptime_str
-                if int(months) > 0:
-                    uptime_str = f"{int(months)} month{'' if int(months) == 1 else 's'} " + uptime_str
+            uptime_str = ""
+            if int(uptime) % 60 > 0:
+                uptime_str = f"{int(uptime) % 60} second{'' if int(uptime) % 60 == 1 else 's'}"
+            if int(minutes) % 60 > 0:
+                uptime_str = f"{int(minutes) % 60} minute{'' if int(minutes) % 60 == 1 else 's'} " + uptime_str
+            if int(hours) % 24 > 0:
+                uptime_str = f"{int(hours) % 24} hour{'' if int(hours) % 24 == 1 else 's'} " + uptime_str
+            if int(days) % 60 > 0:
+                uptime_str = f"{int(days) % 7} day{'' if int(days) % 7 == 1 else 's'} " + uptime_str
+            if int(weeks) % 4.28 > 0:
+                uptime_str = f"{int(weeks) % 4.28} week{'' if int(weeks) % 4.28 == 1 else 's'} " + uptime_str
+            if int(months) > 0:
+                uptime_str = f"{int(months)} month{'' if int(months) == 1 else 's'} " + uptime_str
 
-                embed = discord.Embed(title="Bot status", description="Current bot status",
-                                      color=discord.Color.green())
-                embed.add_field(name="latency", value=f"{ping*1000:.2f} ms", inline=False)
-                embed.add_field(name="uptime", value=uptime_str, inline=False)
+            embed = discord.Embed(title="Bot status", description="Current bot status",
+                                  color=discord.Color.green())
+            embed.add_field(name="latency", value=f"{ping*1000:.2f} ms", inline=False)
+            embed.add_field(name="uptime", value=uptime_str, inline=False)
 
-                await interaction.response.send_message(embed=embed)
+            await interaction.response.send_message(embed=embed)
+        elif choices.value == "host":
+            pass
+        else:
+            pass
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(EXTStatus(client))
